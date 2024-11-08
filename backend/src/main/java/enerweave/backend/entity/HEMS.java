d8cecfebdc0fcdaaac4fecabaaf8c6773b411f05
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-11-08 20:25:22 +0200

    backend ADD: entity annotation and id fields for hems and hardware classes

--- backend/src/main/java/enerweave/backend/entity/HEMS.java
+++ backend/src/main/java/enerweave/backend/entity/HEMS.java
@@ -1,5 +1,9 @@
 package enerweave.backend.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,8 +13,16 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class HEMS {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
     String name;
+
     String producer;
+
     List<Hardware> supportedHardwares;
 }
--- backend/src/main/java/enerweave/backend/entity/Hardware.java
+++ backend/src/main/java/enerweave/backend/entity/Hardware.java
@@ -1,5 +1,9 @@
 package enerweave.backend.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,8 +11,16 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class Hardware {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
     String model;
+
     String category;
+
     String producer;
 }
